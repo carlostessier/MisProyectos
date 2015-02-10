1ab7291ea256c1d812fb863b4c6c8d6fd2696f87
carlosfernandez <prof.carlos.tessier@gmail.com>
2015-02-10 21:31:54 +0100

    changed build tools

--- app/src/main/java/es/tessier/carlos/misproyectos/Compartir.java
+++ app/src/main/java/es/tessier/carlos/misproyectos/Compartir.java
@@ -1,56 +1,40 @@
 package es.tessier.carlos.misproyectos;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.EditText;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ShareActionProvider;
+import android.widget.TextView;
 
 
 public class Compartir extends Activity {
 
-    private static final String TAG = Compartir.class.getName() ;
-    private ShareActionProvider mShareActionProvider;
+    private static final String TAG = Compartir.class.getName();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compartir);
-    }
-
-
-
-    private Intent createShareCompartirIntent(){
-        EditText editText = (EditText) findViewById(R.id.editText);
-        Intent shareIntent = new Intent(Intent.ACTION_SEND);
-        shareIntent.addFlags((Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET));
-        shareIntent.setType("text/plain");
-        shareIntent.putExtra(Intent.EXTRA_TEXT, editText.getText().toString());
-
-        return shareIntent;
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate menu resource file.
+        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_compartir, menu);
-
-        // Locate MenuItem with ShareActionProvider
-        MenuItem item = menu.findItem(R.id.action_share);
-
-        // Fetch and store ShareActionProvider
-        //mShareActionProvider = (ShareActionProvider) item.getActionProvider();
-        mShareActionProvider = (ShareActionProvider) item.getActionProvider();
-        // Return true to display menu
-
-        if (mShareActionProvider!=null)
-            mShareActionProvider.setShareIntent(createShareCompartirIntent());
-        else
-            Log.e(TAG,"shareactionprovider is null!");
         return true;
     }
 
@@ -60,10 +44,68 @@ public class Compartir extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-
-
+        if (id == R.id.action_settings) {
+            return true;
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        private final static String DAM_HASHTAG = "#DAM";
+        private String mTweetString ;
+
+        public PlaceholderFragment() {
+            setHasOptionsMenu(true);
+
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_compartir, container, false);
+
+            Intent intent = getActivity().getIntent();
+            if(intent != null && intent.hasExtra(Intent.EXTRA_TEXT)){
+                mTweetString = intent.getStringExtra(Intent.EXTRA_TEXT);
+                ((TextView) rootView.findViewById(R.id.editText)).setText(mTweetString);
+                //Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
+            }
+
+            return rootView;
+        }
+
+        private Intent createShareIntent(){
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.addFlags((Intent.FLAG_ACTIVITY_NEW_DOCUMENT));
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT,mTweetString+DAM_HASHTAG);
+
+            return shareIntent;
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            inflater.inflate(R.menu.menu_compartir_fragment, menu);
+
+            //Retrieve the share menu item
+            MenuItem menuItem = menu.findItem(R.id.action_share);
+
+            // Get the provider and hold onto it to set/change the share intent.
+            ShareActionProvider mShareActionProvider = (ShareActionProvider) menuItem.getActionProvider();
+
+            // Attach an intent to this ShareActionProvider. You can update this at any time,
+            // like when the user selects a new piece a new piece of data they might like to share
+            if(mShareActionProvider != null) {
+                mShareActionProvider.setShareIntent(createShareIntent());
+
+            }
+            else {
+                Log.d(TAG, "Share action provider is null?");
+            }
+        }
     }
 }
---
+++ app/src/main/java/es/tessier/carlos/misproyectos/Compartir2.java
@@ -0,0 +1,69 @@
+package es.tessier.carlos.misproyectos;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ShareActionProvider;
+
+
+public class Compartir2 extends Activity {
+
+    private static final String TAG = Compartir2.class.getName() ;
+    private ShareActionProvider mShareActionProvider;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compartir2);
+    }
+
+
+
+    private Intent createShareCompartirIntent(){
+        EditText editText = (EditText) findViewById(R.id.editText);
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags((Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET));
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, editText.getText().toString());
+
+        return shareIntent;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate menu resource file.
+        getMenuInflater().inflate(R.menu.menu_compartir2, menu);
+
+        // Locate MenuItem with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.action_share);
+
+        // Fetch and store ShareActionProvider
+        //mShareActionProvider = (ShareActionProvider) item.getActionProvider();
+        mShareActionProvider = (ShareActionProvider) item.getActionProvider();
+        // Return true to display menu
+
+        if (mShareActionProvider!=null)
+            mShareActionProvider.setShareIntent(createShareCompartirIntent());
+        else
+            Log.e(TAG,"shareactionprovider is null!");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+
+        return super.onOptionsItemSelected(item);
+    }
+}
