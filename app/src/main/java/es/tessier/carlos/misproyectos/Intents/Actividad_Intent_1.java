4c4addde14c87b76888b41c90bc2d5c6f326869b
Tessier <prof.carlos.tessier@gmail.com>
2018-03-11 20:24:59 +0100

    updated

--- app/src/main/java/es/tessier/carlos/misproyectos/Intents/Actividad_Intent_1.java
+++ app/src/main/java/es/tessier/carlos/misproyectos/Intents/Actividad_Intent_1.java
@@ -3,8 +3,6 @@ package es.tessier.carlos.misproyectos.intents;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 import es.tessier.carlos.misproyectos.R;
@@ -19,31 +17,10 @@ public class Actividad_Intent_1 extends Activity {
     }
 
     public void onClick(View boton){
-        Intent intent = new Intent(this,Actividad_Intent_1_2.class);
+        Intent intent = new Intent(this, es.tessier.carlos.misproyectos.intents.Actividad_Intent_1_2.class);
         startActivity(intent);
 
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_actividad__intent_1, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
--- app/src/main/java/es/tessier/carlos/misproyectos/Intents/Actividad_Intent_2_2.java
+++ app/src/main/java/es/tessier/carlos/misproyectos/Intents/Actividad_Intent_2_2.java
@@ -3,8 +3,6 @@ package es.tessier.carlos.misproyectos.intents;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -32,26 +30,4 @@ public class Actividad_Intent_2_2 extends Activity {
         finish();
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_actividad__intent_2_2, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
